96d875675daeb78a3a5c6f49bcdc90b22453c1fe
WarpPrime <77132826+WarpPrime@users.noreply.github.com>
2022-04-19 09:06:51 -0400

    set up some sort of framework

---
+++ calc.js
@@ -0,0 +1,11 @@
+// currently only supports one stage
+engine = {
+  ispAtm: 230,
+  ispVac: 250,
+  thrust: 200000,
+  engine: "solid", // liquid, solid, hybrid, etc.
+  throttleRange: [1,1], // number, as a float, as the maximum/minimum throttle this engine can take on
+}
+/* liquid engine custom stats
+throttleRange: [1,1], // number, as a float, as the maximum/minimum throttle this engine can take on
+*/
