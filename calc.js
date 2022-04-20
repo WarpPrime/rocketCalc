b624e3aaa6ea4c179eac472a3abdb6998aff6169
WarpPrime <77132826+WarpPrime@users.noreply.github.com>
2022-04-19 22:31:21 -0400

    barometric pressure

--- calc.js
+++ calc.js
@@ -27,3 +27,21 @@ top: 7.15853832487*(t**3) - 14.2817979947*(t**2) + 7.12283717675*t + 0.000451083
 bottom: 12.7998297531*(t**6) - 39.9195507656*(t**5) + 44.0987641152*(t**4) - 11.3149390159*(t**3) - 13.6170556358*(t**2) + 7.95567673071*t + 0.00451079034958
 average thrust: 1.00596096919 of rated thrust
 */
+function pressure(h) {
+  // use equations in desmos
+  else if (h>=83000 && h<250000) { // thermosphere
+    pressure = 0.0005;
+  }
+  else { // exosphere, don't want to deal with infinitesimals that annoy calculation with IEEE 754
+    pressure = 0;
+  }
+}
+
+function P1(refP, refT, lapse, height, refH) {
+  if (lapse == 0) {
+    return refP * ((refT + (height-refH)*lapse)/refT) ** ((-9.80665*0.0289644)/(8.3144598*lapse));
+  }
+  else {
+    return refP * Math.exp((-9.80665*0.0289644*(height-refH))/(8.3144598*refT));
+  }
+}
